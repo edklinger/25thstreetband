a6cd2e8013d2aaeae678fdce02d279cf67201212
Your Name <your-github-email@example.com>
2025-02-22 17:04:39 +0000

    Update Next.js and Netlify configuration for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     remotePatterns: [
       {
