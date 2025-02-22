9ce61d4a8334daae314cab4fe4aa1588707f439b
Your Name <your-github-email@example.com>
2025-02-22 17:22:38 +0000

    Replace all Font Awesome icons with SVG alternatives to fix type issues

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import Navigation from '../components/Navigation'
-import { FaEnvelope, FaPhone } from 'react-icons/fa'
-import { IconType } from 'react-icons'
 import Image from 'next/image'
 import React from 'react'
 
