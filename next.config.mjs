762027b74f0a26afe26a7fb6bc465bff75af598a
Your Name <your-github-email@example.com>
2025-02-22 00:47:22 +0000

    Drastically simplify project: remove all unnecessary dependencies and configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "replicate.com",
-      },
-      {
-        protocol: "https",
-        hostname: "replicate.delivery",
-      },
-      {
-        protocol: "https",
-        hostname: "www.gstatic.com",
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://api.openai.com/:path*",
-      },
-    ];
-  },
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
